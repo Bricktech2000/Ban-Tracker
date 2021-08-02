fad3d4f3aad498bd030e65f0aa986c90c7a09485
Bricktech <Bricktech2000@gmail.com>
2021-08-02 13:13:41 -0400

    improved tracking using cookies

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import geoip from 'geoip-lite';
 import uaparser from 'ua-parser-js';
+import cookieParser from 'cookie-parser';
 const app = express();
 
 const port = 90;
 const threshold = 8;
+const cookieName = '_ban_tracker_id';
 
 var db = {};
+app.use(cookieParser());
 app.get('/', async function (req, res) {
   //https://www.npmjs.com/package/ua-parser-js
   //https://github.com/geoip-lite/node-geoip
   //https://stackoverflow.com/questions/17781472/how-to-get-a-subset-of-a-javascript-objects-properties
+  //https://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server
 
   //exclude: ua
   var { ua, ...rest } = uaparser(req.headers['user-agent']);
@@ -21,8 +25,13 @@ app.get('/', async function (req, res) {
     geoip.lookup(req.connection.remoteAddress) || {};
   var ip = { region, city, timezone, ll };
 
+  //include cookie (and double its weight)
+  var ck = {};
+  ck.ck1 = req.cookies[cookieName] || '';
+  ck.ck2 = ck.ck1;
+
   //combine ip and ua
-  var obj = { ua, ip };
+  var obj = { ua, ip, ck };
 
   var id = null;
   Object.entries(db).forEach(([key, objs]) => {
@@ -45,6 +54,8 @@ app.get('/', async function (req, res) {
 
   if (typeof req.query.clear !== 'undefined') {
     delete db[id];
+    //https://stackoverflow.com/questions/27978868/destroy-cookie-nodejs
+    res.cookie(cookieName, '', { maxAge: 0 });
     res.redirect('/');
   } else {
     var html = `
@@ -67,20 +78,22 @@ app.get('/', async function (req, res) {
     if (id === null) {
       id = genRandomHex(256 / 16);
       db[id] = [obj];
-      res.end(
-        html.replace(
-          '<MESSAGE>',
-          `<h3>You are a new user<br>ID: ${id}</h3> If this is not the first time visiting this site and haven't cleared your ban, you just bypassed the system! Your status has now been changed to 'banned'. Refresh the page once you have read this message.`
-        )
+      html = html.replace(
+        '<MESSAGE>',
+        `<h3>You are a new user<br>ID: ${id}</h3> If this is not the first time visiting this site and haven't cleared your ban, you just bypassed the system! Your status has now been changed to 'banned'. Refresh the page once you have read this message.`
       );
     } else {
-      res.end(
-        html.replace(
-          '<MESSAGE>',
-          `<h3>You are a known user<br>ID: ${id}</h3> Your status is still 'banned' and you haven't bypassed the system. <a href="?clear">Simulate a ban appeal</a>`
-        )
+      html = html.replace(
+        '<MESSAGE>',
+        `<h3>You are a known user<br>ID: ${id}</h3> Your status is still 'banned' and you haven't bypassed the system. <a href="?clear">Simulate a ban appeal</a>`
       );
     }
+    res.cookie(cookieName, id, {
+      maxAge: 1000 * 60 * 60 * 24 * 100, //expire after 100 years
+      httpOnly: true, //make the cookie only accessible by the web server
+      signed: false, //sign the cookie
+    });
+    res.end(html);
   }
 
   // console.log(db);
