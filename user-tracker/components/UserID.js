a3d287826b267cd8adc6f2e77ec611be1181e5bc
Bricktech <Bricktech2000@gmail.com>
2021-08-05 19:54:43 -0400

    made UserID component change color according to User ID

--- user-tracker/components/UserID.js
+++ user-tracker/components/UserID.js
@@ -5,7 +5,16 @@ import styles from './UserID.module.css';
 class UserID extends Component {
   state = {};
   render() {
-    return <div className={styles.UserID}>{this.props.id}</div>;
+    return (
+      <div
+        className={styles.UserID}
+        style={{
+          background: `hsl(${parseInt(this.props.id) % 360}, 100%, 60%)`,
+        }}
+      >
+        {this.props.id}
+      </div>
+    );
   }
 }
 
